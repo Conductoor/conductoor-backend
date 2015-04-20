a268562c48331ee996125f8f61b3dea5b5af2a19
Ville Tainio <tainio.ville@gmail.com>
2015-04-20 11:13:09 +0300

    Add knows field for user serialization

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from .models import User
+from skills.serializers import SkillSerializer
 
 class UserSerializer(serializers.ModelSerializer):
+  knows = SkillSerializer(many=True, read_only=True)
+
   class Meta:
     model = User
-    fields = ('id', 'email', 'first_name', 'last_name')
+    fields = ('id', 'email', 'first_name', 'last_name', 'knows')
