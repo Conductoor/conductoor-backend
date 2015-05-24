bb40a3566f414fa4d3f434012c84a7d2f758c974
Ville Tainio <tainio.ville@gmail.com>
2015-05-24 22:36:54 +0300

    Remove change-password from api root

--- projects/views.py
+++ projects/views.py
@@ -83,5 +83,4 @@ class APIRoot(APIView):
       'available-users': reverse('available-users', request=request),
       'request-user': reverse('request-user', request=request),
       'request-beta': reverse('request-beta', request=request),
-      'show-beta': reverse('show-beta', request=request),
-      'change-password': reverse('change-password', request=request)})
+      'show-beta': reverse('show-beta', request=request)})
